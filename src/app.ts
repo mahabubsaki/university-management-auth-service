a4760f65ad376af8a1e2491869ac455ab31e6b9b
mahabubsaki <md929820@gmail.com>
2023-06-30 04:11:58 +0600

    minor file change for removing dist folder from github

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 const app: Application = express();
 
-//middleare
+//middleare and parser
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
