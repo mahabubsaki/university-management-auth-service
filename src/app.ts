65ebcc6543da3907375986b60ce9899fa023eb25
mahabubsaki <md929820@gmail.com>
2023-06-30 05:07:41 +0600

    s

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 //testing route
-app.get('/', (req: Request, res: string) => {
+app.get('/', (req: Request, res: Response) => {
   res.send({ message: 's' })
 })
 
