5e21f6c3bcbcb6ba224be3ff4bde2353097abac9
mahabubsaki <md929820@gmail.com>
2023-06-30 04:40:54 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
-const app: Application = express();
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
 //middleare and parser
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //testing route
 app.get('/', (req: Request, res: Response) => {
-    res.send({ message: 's' });
-});
+  res.send({ message: 's' })
+})
 
-export default app;
+export default app
