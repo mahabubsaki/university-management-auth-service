e88d2c96618a2bd769f72290add8ec21dcf1d53c
mahabubsaki <md929820@gmail.com>
2023-06-30 03:21:57 +0600

    [DONE] Created env and setup parser

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function main() {
+    try {
+        await mongoose.connect(config.dbUri as string);
+        app.listen(config.port, () => {
+            console.log('Listening to ', config.port);
+        });
+    }
+    catch (err) {
+        console.log('failed to connect db', err);
+    }
+}
+main();
