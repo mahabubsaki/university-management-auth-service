06b818a95cb2a4913db5e9d7521946119f43d13f
mahabubsaki <md929820@gmail.com>
2023-06-30 05:42:21 +0600

    [DONE] Created user interface,schema and model

---
+++ src/modules/users/user.interface.ts
@@ -0,0 +1,15 @@
+import { Model } from 'mongoose'
+
+export interface IUser {
+  id: string
+  role: string
+  password: string
+}
+
+export interface IUserMethods {
+  demo: () => string
+}
+
+export interface IUserStatics extends Model<IUser, object, IUserMethods> {
+  demo: () => string
+}
