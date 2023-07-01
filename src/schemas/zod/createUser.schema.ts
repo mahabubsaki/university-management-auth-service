4c6f2e4c1643def38dbd60084fb137364f036d4a
mahabubsaki <md929820@gmail.com>
2023-07-01 19:01:12 +0600

    [DONE] implemented zod validation

--- src/schemas/zod/createUser.schema.ts
+++ src/schemas/zod/createUser.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const createUserZodSchema = z.object({
     body: z.object({
         role: z.string({
-            required_error: "Role is required for creating user"
+            required_error: "Role is required for creating user (need string)"
         }),
         password: z.string().optional()
     })
