d318a10ab49b4abbe5ec2dfa8c98d67dab1c49c7
mahabubsaki <md929820@gmail.com>
2023-07-01 17:36:43 +0600

    fixed winston logger and handled unhandledrejection

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,7 @@ import path from 'path';
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
+  env: process.env.NODE_ENV,
   port: process.env.PORT,
   dbUri: process.env.DATABASE_URL,
   default_user_password: process.env.DEAFAULT_USER_PASSWORD
